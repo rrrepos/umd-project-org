9d1c9918d63c5c4f931a336e7342a152226a5801
RmR <connect2rmr@gmail.com>
2021-02-07 08:36:32 +0530

    changes to snowpack.config

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     optimize: {
         bundle: true,
         minify: true,
-        target: 'es2018',
+        target: 'es2020',
     },
     buildOptions: {
         out: "docs",
---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+// app.js
+//
+import * as filer from "./modules/filer.js";
+
+const setSw = () => {
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register("../sw.js")
+            .then(function (registration) {
+                navigator.serviceWorker.addEventListener("message", event => {
+                    //window.location.reload();
+                });
+            }).catch(function (error) {
+                console.log("ServiceWorker registration failed:", error);
+            });
+    }
+}
+
+document.querySelector(".main-container").removeAttribute("hidden");
+//
+filer.setEvents();
+// 
+//setSw();
+//
