1a78934aaf4a0db56c683bf42631b28ae2be0611
RmR <connect2rmr@gmail.com>
2021-02-09 19:22:15 +0530

    changed alerts

--- src/js/app.js
+++ src/js/app.js
@@ -21,9 +21,10 @@ filer.setEvents();
 setSw();
 //
 
+/*
 window.addEventListener("message", message => {
     console.log(message.data) // Wayne is coming!!!
     alert("received from react");
     alert(message.data);
   });
-  
+*/  
