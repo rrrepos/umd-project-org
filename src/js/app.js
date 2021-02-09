19476bdeaf047d8aced6f0de548069a534d30b6e
RmR <connect2rmr@gmail.com>
2021-02-09 18:18:17 +0530

    added message

--- src/js/app.js
+++ src/js/app.js
@@ -20,3 +20,7 @@ filer.setEvents();
 // 
 setSw();
 //
+window.addEventListener("message", message => {
+    console.log(message.data) // Wayne is coming!!!
+    window.ReactNativeWebView.postMessage('Client received data')
+  });
